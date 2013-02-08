db4e767c12e2280c17fab8b7fdcba7fd0a51e141
aaron-david <aaron.dsgn@gmail.com>
2013-02-08 00:03:47 -0200

    Antes da merda do bootstrap
    Vamos ver no que vai dar

---
+++ como-chegar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<?php include 'head.php'; ?>
+	<title>PostNet Curitiba</title>
+</head>
+<body>
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+	
+	<div id="main" class="clearfix">
+		<?php include 'header.php'; ?>
+		<!-- Início de #container -->
+		<div id="container" class="container_12">
+			<?php include 'header-destaque.php'; ?>
+			<div id="" class="red_line_top clearfix">
+				
+			</div>
+			<?php include 'footer.php'; ?>
+		</div> <!-- Fim de #container -->
+	</div> <!-- Fim de #main -->
+</body>
+</html> 

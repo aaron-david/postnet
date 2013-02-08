9747b80903e7084b5620207490e54d16450a84c0
aaron-david <aaron.dsgn@gmail.com>
2013-02-08 00:10:15 -0200

    Revert "Revert "Revert "Antes da merda do bootstrap"""
    This reverts commit 50278aff865a6962b8ca30cd7e43e3af5e73ed8c.

--- enviado.php
+++ enviado.php
@@ -1,31 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-	<?php include 'head.php'; ?>
-	<title>PostNet Curitiba</title>
-</head>
-<body>
-	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script>
-	
-	<div id="main" class="clearfix">
-		<?php include 'header.php'; ?>
-		<!-- Início de #container -->
-		<article id="container" class="container_12">
-			<?php include 'header-destaque.php'; ?>			
-			<section id="enviado" class="red_line_top clearfix">
-				<div class="grid_12 well">
-					Sua mensagem foi enviada com sucesso!
-				</div>
-			</section>
-			<?php include 'footer.php'; ?>
-		</article> <!-- Fim de #container -->
-	</div> <!-- Fim de #main -->
-</body>
-</html> 
+<html>
+	<head>
+		<title>Mensagem enviada</title>
+	</head>
+	<body>
+		<p class="well">Sua mensagem foi enviada com sucesso!</p>
+	</body>
+</html>
